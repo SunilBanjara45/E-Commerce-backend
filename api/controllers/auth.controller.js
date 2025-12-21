95bf252b70c75a8696980cac2736fc19d8230c80
Sunil Banjara <sunilbanjara441@gmail.com>
2025-12-22 02:23:06 +0530

    handleError utils and signup controller created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,15 +2,17 @@ const User = require('../models/user.schema')
 const httpStatus = require('http-status')
 const { matchedData } = require('express-validator')
 const { buildErrorObject } = require('../utils/buildErrorObject')
+const { buildResponse } = require('../utils/buildResponse')
+const { handleError } = require('../utils/handleError')
 
 
 // signup controller
 
-exports.signupController = async(req, res) =>{
+exports.signupController = async (req, res) => {
     try {
         const data = matchedData(req)
         const existingUser = await User.findOne('phoneNumber')
-        if(existingUser){
+        if (existingUser) {
             throw buildErrorObject(
                 httpStatus.CONFLICT,
                 "User Already Registered "
@@ -19,10 +21,12 @@ exports.signupController = async(req, res) =>{
 
         await User.create(req)
 
-        res.status(httpStatus.CREATED).json(b)
-        
-    } 
-    catch (error) {
-        
+        res.status(httpStatus.CREATED).json(buildResponse(httpStatus.httpStatus.CREATED, {
+            message: 'User Created Successfully'
+        }))
+
+    }
+    catch (err) {
+        handleError(res, err)
     }
 }
---
+++ api/utils/handleError.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+exports.handleError = (res, err) => {
+    if (process.env.NODE_ENV === 'development') {
+        console.log(err)
+    }
+
+    const response = {
+        success: false,
+        code: 500,
+        message: 'SOMETHING WENT WRONG'
+    }
+    if (err?.message) {
+        response.message = err.message
+    }
+    if (err?.code <= 500) {
+        response.code = err.code
+    }
+
+    res.status(response.code).json(response)
+}
